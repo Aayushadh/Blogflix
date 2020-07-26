9969f18ea078dce7d95bd1caad47a6e24c84731d
Aaysuhadh <ayushharwani2011@gmail.com>
2020-07-26 22:28:37 +0530

    prototype

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.trial, name='trial'),
+    path('index/',views.post_list,name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
